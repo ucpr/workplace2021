37907be4f9cee4ccd4fb1e85dc3e53a2bedfff2f
ucpr <contact@ucpr.dev>
2021-08-13 15:16:26 +0900

    feat: test go prometheus custom metrics

---
+++ go_prometheus/go.mod
@@ -0,0 +1,12 @@
+module github.com/ucpr/workspace2021/go_prometheus
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/prometheus/client_golang v1.11.0 // indirect
+	github.com/prometheus/common v0.30.0 // indirect
+	github.com/prometheus/procfs v0.7.2 // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ go_prometheus/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	httpReqs = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "How many HTTP requests processed, partitioned by status code and HTTP method.",
+		},
+		[]string{"code", "method"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(httpReqs)
+}
+
+func main() {
+	http.Handle("/", http.HandlerFunc(count))
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func count(w http.ResponseWriter, r *http.Request) {
+	a := httpReqs.WithLabelValues("test", "1")
+	b := httpReqs.WithLabelValues("test", "2")
+	a.Inc()
+	b.Inc()
+}
